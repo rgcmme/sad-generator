244dbf70480f4a96909f1e2c030e1878548cde88
dachafra <dachafra@gmail.com>
2019-12-23 10:13:57 +0100

    change uri of event

--- website/_data/importantdates.js
+++ website/_data/importantdates.js
@@ -11,7 +11,7 @@ module.exports = async () => {
     "date": { "@id": "http://schema.org/startDate", "@singular": true },
     "subEvent": { "@reverse": "http://schema.org/subEvent", "@singular": true },
     "Event": "http://schema.org/Event",
-    "workshop": "http://example.com/resources/Event/example-event"
+    "workshop": "http://example.com/resources/Event/sedit2020"
   },`{... on Event {title description date subEvent(_:workshop)}}`);
 
   result.sort( (a, b) => {
